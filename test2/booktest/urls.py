d7ce26fc497adc121535c3c59e8224de5519ba6f
admin <jacktony822@gmail.com>
2020-12-30 19:39:16 +0800

    提交

--- test2/booktest/urls.py
+++ test2/booktest/urls.py
@@ -5,4 +5,5 @@ from booktest import views
 
 urlpatterns = [
     url(r'^index$', views.index),
+    url(r'^create$', views.create),
 ]
--- test2/booktest/views.py
+++ test2/booktest/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from booktest.models import BookInfo
+from datetime import date
+from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
 def index(request):
     '''显示图书信息'''
@@ -8,3 +10,14 @@ def index(request):
     #使用模板
     return  render(request, 'booktest/index.html',
                    {'books':books})
+
+def create(request):
+    ''''新增一本图书'''
+    #创建BookInfo对象
+    b = BookInfo()
+    b.btitle = '流星蝴蝶剑'
+    b.bpub_date = date(1990,1,1)
+    #保存
+    b.save()
+    #返回应答
+    return HttpResponseRedirect('/index')
