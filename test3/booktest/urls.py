1b13b186cf653a3e3245cf96746d2812cf992f7d
admin <jacktony822@gmail.com>
2021-01-08 19:06:10 +0800

    0107更新

--- test3/booktest/urls.py
+++ test3/booktest/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     url(r'^ajax_handle$', views.ajax_handle),
     url(r'^login_ajax$', views.login_ajax), #显示ajax登录页面
     url(r'^login_ajax_check$', views.login_ajax_check), # ajax登录校验
+    url(r'^set_cookie$', views.set_cookie), #设置cookie
+    url(r'^get_cookie$', views.get_cookie), #获取cookie
 ]
--- test3/booktest/views.py
+++ test3/booktest/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, JsonResponse
+from datetime import datetime,timedelta
 # Create your views here.
 def index(request):
     # num = 'a' + 1
@@ -9,17 +10,33 @@ def show_arg(request, num):
     return HttpResponse(num)
 
 def login(request):
-    return render(request, "booktest/login.html")
+    '''获取cookie.username'''
+    if 'username' in request.COOKIES:
+        #获取记住的用户名
+        username = request.COOKIES['username']
+    else:
+        username = ''
+    return render(request, "booktest/login.html",
+            {'username':username})
 
 def login_check(request):
     '''登录校验视图'''
     #获取提交的用户名和密码
     username = request.POST.get('username')
     password = request.POST.get('password')
-    print(username + ':' + password)
+    remember = request.POST.get('remember')
+    # print(remeber)
+    # print(username + ':' + password)
     #进行登陆的校验
     if username == 'admin' and password == '123456':
-        return redirect('/index')
+        #用户名密码正确，跳转到首页
+        response =  redirect('/index')
+        #判断是否需要记住用户名
+        if remember == 'on':
+            #设置cookie
+            response.set_cookie('username',username,
+                    max_age=7*24*3600)
+        return response
     else:
     #返回应答
         return redirect('/login')
@@ -46,3 +63,15 @@ def login_ajax_check(request):
         return JsonResponse({'res':1})
     else:
         return JsonResponse({'res':0})
+
+def set_cookie(request):
+    '''设置cookie信息'''
+    response = HttpResponse('设置cookie')
+    response.set_cookie('num', 1, max_age=14*24*3600)
+    # response.set_cookie('num', 1,expires=datetime.now()+timedelta(dats=14))
+    return response
+
+def get_cookie(request):
+    '''获取cookie信息'''
+    num = request.COOKIES['num']
+    return HttpResponse(num)
