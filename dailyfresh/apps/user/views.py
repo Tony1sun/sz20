7bc082f1d697174270e1e8b704a3323078737f49
admin <jacktony822@gmail.com>
2021-01-22 18:36:21 +0800

    0122

---
+++ dailyfresh/apps/user/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from user.models import User
+import re
+# Create your views here.
+
+# /user/register
+def register(request):
+    '''显示注册页面'''
+    return render(request, 'register.html')
+
+def register_handle(request):
+    '''处理注册'''
+    #接收数据
+    username = request.POST.get('user_name')
+    password = request.POST.get('pwd')
+    email = request.POST.get('email')
+    allow = request.POST.get('allow')
+
+    #进行数据校验
+    if not all([username, password, email]):
+        #数据不完整
+        return render(request, 'register.html', {'errmsg':'数据不完整'})
+    #校验邮箱
+    if not re.match(r'^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+        return render(request, 'register.html', {'errmsg':'邮箱格式不正确'})
+
+    if allow != 'on':
+        return render(request, 'register.html', {'errmsg':"请同意协议"})
+    #进行业务处理：注册
+    user = User.objects.create_user(username, password, email)
+
+    #返回应答,跳转到首页
+    return redirect(reverse('goods:index'))
