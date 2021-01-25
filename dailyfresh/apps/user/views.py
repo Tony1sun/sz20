f1305509f50154d5b06d227510b1d2ac86712bae
admin <jacktony822@gmail.com>
2021-01-25 19:32:05 +0800

    0125

--- dailyfresh/apps/user/views.py
+++ dailyfresh/apps/user/views.py
@@ -4,8 +4,9 @@ from django.core.mail import send_mail
 from django.views.generic import View
 from django.http import HttpResponse
 from django.conf import settings
-from user.models import User
 
+from user.models import User
+from celery_tasks.tasks import send_register_active_email
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import SignatureExpired
 import re
@@ -139,13 +140,8 @@ class RegisterView(View):
         token = token.decode()
 
         #发邮件
-        subject = '天天生鲜欢迎信息'
-        message = ''
-        sender = settings.EMAIL_FROM
-        receiver = [email]
-        html_message = '<h1>%s，欢迎您成为天天生鲜注册会员</h1>请点击下面链接激活您的账户<br/><a href="http://127.0.0.1:8000/user/active/%s">http://127.0.0.1:8000/user/active/%s</a>'%(username, token, token)
+        send_register_active_email.delay(email, username, token)
 
-        send_mail(subject, message, sender, receiver, html_message=html_message)
         # 返回应答,跳转到首页
         return redirect(reverse('goods:index'))
 
